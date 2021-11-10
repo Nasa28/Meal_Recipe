a8742c1a11c76afb737c632e7aafa01b3447a513
Nasa28 <kaluchinasaagu@gmail.com>
2021-11-11 00:14:35 +0100

    Add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import '../styles/App.css';
-import './NavBar'
-import NavBar from './NavBar';
+import NavBar from '../components/NavBar';
+import mealList from '../components/mealList';
+import mealRecipe from '../components/mealRecipe';
 function App() {
-  return <div className="App">
-    <NavBar />
-  </div>;
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" exact component={mealList} />
+          <Route path="/meal/:mealId" component={mealRecipe} />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
