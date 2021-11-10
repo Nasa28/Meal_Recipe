db140841b05d97da2c5af119415af2ae6f962443
Nasa28 <kaluchinasaagu@gmail.com>
2021-11-10 23:02:14 +0100

    Created the actions

---
+++ src/Redux/Actions/recipeActions.js
@@ -0,0 +1,15 @@
+import { actionTypes } from '../constants/actionTypes';
+
+export const setMeals = (meals) => {
+  return {
+    types: actionTypes.SET_MEAL,
+    Payload: meals,
+  };
+};
+
+export const selectedMeal = (meal) => {
+  return {
+    types: actionTypes.SELECTED_MEAL,
+    Payload: meal,
+  };
+};
