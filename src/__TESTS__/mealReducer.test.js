206aae13abc74d7b13c6d70df8f4933b8612d4dc
Nasa28 <kaluchinasaagu@gmail.com>
2021-11-22 19:46:52 +0100

    Add unit test for Reducer

--- src/__TESTS__/mealReducer.test.js
+++ src/__TESTS__/mealReducer.test.js
@@ -3,6 +3,27 @@ import actionTypes from '../Redux/constants/actionTypes';
 import { setMeals } from '../Redux/Actions/mealActions';
 
 describe('Meal Reducer', () => {
+  const meals = [
+    {
+      strMeal: 'Baked salmon with fennel & tomatoes',
+      strMealThumb:
+        'https://www.themealdb.com/images/media/meals/1548772327.jpg',
+      idMeal: '52959',
+    },
+    {
+      strMeal: 'Cajun spiced fish tacos',
+      strMealThumb:
+        'https://www.themealdb.com/images/media/meals/uvuyxu1503067369.jpg',
+      idMeal: '52819',
+    },
+    {
+      strMeal: 'Escovitch Fish',
+      strMealThumb:
+        'https://www.themealdb.com/images/media/meals/1520084413.jpg',
+      idMeal: '52944',
+    },
+  ];
+
   it('Should return default state', () => {
     const newState = reducer(undefined, {});
     expect(newState).toEqual({ myMeals: [] });
@@ -11,5 +32,23 @@ describe('Meal Reducer', () => {
   it('Should return default state', () => {
     const newState = reducer(undefined, {});
     expect(newState).not.toEqual(undefined);
+  });
+
+  it('Should return new state if action type is recieved', () => {
+    const newState = reducer(undefined, {
+      type: actionTypes.SET_MEALS,
+      payload: meals,
+    });
+
+    expect(newState).toEqual({ myMeals: meals });
+  });
+
+  it('Should return new state if action type is recieved', () => {
+    const newState = reducer(undefined, {
+      type: actionTypes.SET_MEALS,
+      payload: meals,
+    });
+
+    expect(newState).not.toEqual(null);
   });
 });
